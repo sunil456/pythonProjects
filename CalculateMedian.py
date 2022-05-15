88345edbd97161424d28e602cc276a7f2c61da27
sunil sharma <sunil.sharma533@gmail.com>
2022-05-15 18:07:47 +0530

    my CalculateMedian

---
+++ CalculateMedian.py
@@ -0,0 +1,7 @@
+"""
+Median
+The Median is the middle value among all the values in sorted order.
+Here we need to calculate the mid-value of all the values in a dataset.
+But before calculating the Median, we need to arrange all the values in sorted order.
+There are two different ways of calculating the median value:
+"""
