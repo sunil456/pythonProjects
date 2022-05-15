f7c78f85decfca54f7dd19a07d069de0f8976930
sunil sharma <sunil.sharma533@gmail.com>
2022-05-15 18:05:19 +0530

    my first class

---
+++ CalculateMean.py
@@ -0,0 +1,11 @@
+"""
+Mean
+The mean is the average value of all the values in a dataset.
+To calculate the mean value of a dataset, we first need to find the sum of all the values
+and then divide the sum of all the values by the total number of values
+"""
+
+# Mean
+list1 = [12, 16, 20, 20, 12, 30, 25, 23, 24, 20]
+mean = sum(list1) / len(list1)
+print(mean)
